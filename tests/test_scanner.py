e6a016726bdba599e6eb76a0e6d505f6c2d59307
zzzzwj <zwj@smail.nju.edu.cn>
2021-02-03 17:37:23 +0800

    Add constraint of uint8

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -5,6 +5,7 @@ from deepforest.scanner import ImageScanner
 
 def _prepare_data():
     raw_mat = torch.from_numpy(np.arange(1, 19, 1)).view(1, 2, 3, 3)
+    raw_mat = raw_mat.byte()
 
     padding_mat = raw_mat.new_zeros((1, 2, 5, 5))
     padding_mat[:, :, 1:4, 1:4] = raw_mat
