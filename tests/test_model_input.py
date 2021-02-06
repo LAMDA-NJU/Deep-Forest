93c9b6c14146042515873959777ef6ee559ff7aa
Yi-Xuan Xu <xuyx@lamda.nju.edu.cn>
2021-02-06 12:14:57 +0800

    black formatting

--- tests/test_model_input.py
+++ tests/test_model_input.py
@@ -4,10 +4,9 @@ from numpy.testing import assert_array_equal
 from sklearn.datasets import load_digits
 from deepforest import CascadeForestClassifier
 
+
 def test_model_input_label_encoder():
-    """
-    Test if the model behaves the same with and without label encoding.
-    """
+    """Test if the model behaves the same with and without label encoding."""
 
     # Load data
     X, y = load_digits(return_X_y=True)
@@ -24,7 +23,9 @@ def test_model_input_label_encoder():
     y_pred_str_labels = model.predict(X)
 
     # Check if the underlying data are the same
-    y_pred_int_labels_as_str = np.char.add("label_", y_pred_int_labels.astype(str))
+    y_pred_int_labels_as_str = np.char.add(
+        "label_", y_pred_int_labels.astype(str)
+    )
     assert_array_equal(y_pred_str_labels, y_pred_int_labels_as_str)
 
     # Clean up buffer
