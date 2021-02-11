d20a2fa6a25cd7cbfdf36c3110f92b4d458404cc
Yi-Xuan Xu <xuyx@lamda.nju.edu.cn>
2021-02-11 11:18:49 +0800

    [MNT] Update version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.rst") as f:
 MAINTAINER = "Yi-Xuan Xu"
 MAINTAINER_EMAIL = "xuyx@lamda.nju.edu.cn"
 URL = "https://github.com/LAMDA-NJU/Deep-Forest"
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 
 
 def configuration(parent_package="", top_path=None):
