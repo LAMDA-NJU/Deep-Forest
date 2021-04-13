48ef524f1fade8576a88304c19f0ef6f6fba3c9b
xuyxu <xuyx@lamda.nju.edu.cn>
2021-04-13 13:08:11 +0800

    add unit tests

--- tests/test_set_custom_estimator.py
+++ tests/test_set_custom_estimator.py
@@ -1,8 +1,9 @@
 import pytest
 import shutil
+import numpy as np
 from numpy.testing import assert_array_equal
-from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_iris, load_boston
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 
 from deepforest import CascadeForestClassifier, CascadeForestRegressor
@@ -12,12 +13,20 @@ save_dir = "./tmp"
 
 # Load data
 X, y = load_iris(return_X_y=True)
-X_train, X_test, y_train, y_test = train_test_split(
+X_train_clf, X_test_clf, y_train_clf, y_test_clf = train_test_split(
     X, y, test_size=0.42, random_state=42
 )
 
+X, y = load_boston(return_X_y=True)
+X_train_reg, X_test_reg, y_train_reg, y_test_reg = train_test_split(
+    X, y, test_size=0.42, random_state=42
+)
 
-def test_custom_cascade_layer_workflow_in_memory():
+# multi-output target values
+y_train_reg_multi = np.array([y_train_reg, y_train_reg]).reshape(-1, 2)
+
+
+def test_classifier_custom_cascade_layer_workflow_in_memory():
 
     model = CascadeForestClassifier()
 
@@ -28,8 +37,8 @@ def test_custom_cascade_layer_workflow_in_memory():
     predictor = DecisionTreeClassifier()
     model.set_predictor(predictor)
 
-    model.fit(X_train, y_train)
-    y_pred_before = model.predict(X_test)
+    model.fit(X_train_clf, y_train_clf)
+    y_pred_before = model.predict(X_test_clf)
 
     # Save and Reload
     model.save(save_dir)
@@ -38,7 +47,7 @@ def test_custom_cascade_layer_workflow_in_memory():
     model.load(save_dir)
 
     # Predictions after loading
-    y_pred_after = model.predict(X_test)
+    y_pred_after = model.predict(X_test_clf)
 
     # Make sure the same predictions before and after model serialization
     assert_array_equal(y_pred_before, y_pred_after)
@@ -52,7 +61,7 @@ def test_custom_cascade_layer_workflow_in_memory():
     shutil.rmtree(save_dir)
 
 
-def test_custom_cascade_layer_workflow_partial_mode():
+def test_classifier_custom_cascade_layer_workflow_partial_mode():
 
     model = CascadeForestClassifier(partial_mode=True)
 
@@ -63,8 +72,8 @@ def test_custom_cascade_layer_workflow_partial_mode():
     predictor = DecisionTreeClassifier()
     model.set_predictor(predictor)
 
-    model.fit(X_train, y_train)
-    y_pred_before = model.predict(X_test)
+    model.fit(X_train_clf, y_train_clf)
+    y_pred_before = model.predict(X_test_clf)
 
     # Save and Reload
     model.save(save_dir)
@@ -73,7 +82,74 @@ def test_custom_cascade_layer_workflow_partial_mode():
     model.load(save_dir)
 
     # Predictions after loading
-    y_pred_after = model.predict(X_test)
+    y_pred_after = model.predict(X_test_clf)
+
+    # Make sure the same predictions before and after model serialization
+    assert_array_equal(y_pred_before, y_pred_after)
+
+    model.clean()  # clear the buffer
+    shutil.rmtree(save_dir)
+
+
+@pytest.mark.parametrize("y_train", [y_train_reg, y_train_reg_multi])
+def test_regressor_custom_cascade_layer_workflow_in_memory(y_train):
+
+    model = CascadeForestRegressor()
+
+    n_estimators = 4
+    estimators = [DecisionTreeRegressor() for _ in range(n_estimators)]
+    model.set_estimator(estimators)  # set custom base estimators
+
+    predictor = DecisionTreeRegressor()
+    model.set_predictor(predictor)
+
+    model.fit(X_train_reg, y_train)
+    y_pred_before = model.predict(X_test_reg)
+
+    # Save and Reload
+    model.save(save_dir)
+
+    model = CascadeForestRegressor()
+    model.load(save_dir)
+
+    # Predictions after loading
+    y_pred_after = model.predict(X_test_reg)
+
+    # Make sure the same predictions before and after model serialization
+    assert_array_equal(y_pred_before, y_pred_after)
+
+    assert (
+        model.get_estimator(0, 0, "custom")
+        is model._get_layer(0).estimators_["0-0-custom"].estimator_
+    )
+
+    model.clean()  # clear the buffer
+    shutil.rmtree(save_dir)
+
+
+@pytest.mark.parametrize("y_train", [y_train_reg, y_train_reg_multi])
+def test_regressor_custom_cascade_layer_workflow_partial_mode(y_train):
+
+    model = CascadeForestRegressor(partial_mode=True)
+
+    n_estimators = 4
+    estimators = [DecisionTreeRegressor() for _ in range(n_estimators)]
+    model.set_estimator(estimators)  # set custom base estimators
+
+    predictor = DecisionTreeRegressor()
+    model.set_predictor(predictor)
+
+    model.fit(X_train_reg, y_train)
+    y_pred_before = model.predict(X_test_reg)
+
+    # Save and Reload
+    model.save(save_dir)
+
+    model = CascadeForestRegressor()
+    model.load(save_dir)
+
+    # Predictions after loading
+    y_pred_after = model.predict(X_test_reg)
 
     # Make sure the same predictions before and after model serialization
     assert_array_equal(y_pred_before, y_pred_after)
